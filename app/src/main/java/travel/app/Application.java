157ccfa48b6cf9979a42b350f3945d8ad8f72b6d
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2022-11-16 21:02:28 +0100

    scanBasePackages added

--- app/src/main/java/travel/app/Application.java
+++ app/src/main/java/travel/app/Application.java
@@ -3,11 +3,16 @@ package travel.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"travel.app", "travel.controller", "travel.model", "travel.persistence", "travel.security", "travel.service", "travel.validator"})
 @EntityScan("travel.domain")
 @EnableJpaRepositories(basePackages = {"travel.persistence"})
+@SpringBootApplication(scanBasePackages = {
+        "travel.app", "travel.controller", "travel.model",
+        "travel.persistence", "travel.security", "travel.service",
+        "travel.transformer", "travel.validator"
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
