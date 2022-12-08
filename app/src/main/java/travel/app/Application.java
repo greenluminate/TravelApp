a79e4d08ac9fe27eec442f61027d3ed596f75faf
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2022-12-08 10:19:54 +0100

    Code clean up

--- app/src/main/java/travel/app/Application.java
+++ app/src/main/java/travel/app/Application.java
@@ -3,7 +3,6 @@ package travel.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("travel.domain")
